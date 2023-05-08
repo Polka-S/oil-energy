7bb091fd4f25307afaece71d18bffdc45752ffe9
Polka-S <gillson11@yandex.ru>
2023-05-08 18:51:25 +0300

    add some blocks

--- js/burger.js
+++ js/burger.js
@@ -2,8 +2,6 @@ const burger = document.querySelector('#burger');
 const popup = document.querySelector('#popup-menu');
 const body = document.body;
 const menu = document.querySelector('#menu').cloneNode(1);
-// const logo = document.querySelector('#header-logo').cloneNode(1);
-// const btns = document.querySelector('#header__btns').cloneNode(1);
 
 burger.addEventListener("click", burgerHandler);
 function burgerHandler(e) {
@@ -20,20 +18,11 @@ function renderPopup() {
     popup.appendChild(menu);
 }
 
-// function renderPopupTwo() {
-//     popup.appendChild(btns);
-// }
-
 const links = Array.from(menu.children);
-// const tabs = Array.from(btns.children);
 
 links.forEach((link) => {
     link.addEventListener("click", closeOnClick);
 });
-
-// tabs.forEach((tab) => {
-//     tab.addEventListener("click", closeOnClick);
-// })
 
 function closeOnClick() {
     popup.classList.remove("open");
